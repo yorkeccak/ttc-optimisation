a081b44d91d1f4fd0bde9e617bcfc4a8904a49fb
Yamamuchi <harveyyorke@proton.me>
2025-03-16 16:13:20 +0000

    [FEAT] Docstrings + search token

--- llms/llm.py
+++ llms/llm.py
@@ -10,12 +10,15 @@ class Llm(ABC):
         super().__init__()
         self._model = model
         self._valyu = Valyu()
-        self._start_rag = "<RAG>"
-        self._end_rag = "</RAG>"
-        self._start_result = "<RESULT>"
-        self._end_result = "</RESULT>"
+        self._start_rag = "<|begin_search_query|>"
+        self._end_rag = "<|end_search_query|>"
+        self._start_result = "<|begin_search_result|>"
+        self._end_result = "<|end_search_result|>"
 
     def _run_valyu(self: Self, query: str) -> str:
+        """
+        Query the Valyu API to get relevant information for the model.
+        """
         response = self._valyu.context(
             query=query,
             search_type="all",
@@ -26,6 +29,9 @@ class Llm(ABC):
         return response.results[0].content
 
     def _run_ollama(self: Self, prompt: str, stop_tokens=None) -> str:
+        """
+        Runs the model with the given prompt and returns the response.
+        """
         response = ollama.generate(
             model=self._model,
             prompt=prompt,
@@ -35,6 +41,10 @@ class Llm(ABC):
         return response["response"]
 
     def _extract_rag_query(self: Self, text: str) -> str | None:
+        """
+        Extracts the RAG query from the model output.
+        E.g. <|begin_search_query|>What is the capital of France?<|end_search_query|> -> "What is the capital of France?"
+        """
         pattern = re.escape(self._start_rag) + r"(.*?)" + re.escape(self._end_rag)
         matches = re.findall(pattern, text, flags=re.DOTALL)
         return matches[-1].strip() if matches else None
