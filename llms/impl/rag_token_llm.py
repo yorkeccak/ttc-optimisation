bbee9355ce29e539c54a4c5e457947bc63d4e5a4
jadhwani <zcabpmj@ucl.ac.uk>
2025-04-02 22:20:06 +0100

    minor fix

--- llms/impl/rag_token_llm.py
+++ llms/impl/rag_token_llm.py
@@ -4,7 +4,6 @@
 # The search model then uses the Valyu API to search for the information and returns the results.
 # The results are then passed to the reasoning model, which then uses the context to inform the answer.
 
-import time
 from ..llm import Llm
 from typing import Self
 
@@ -109,11 +108,7 @@ If you need additional information, you can search again using {self._start_rag}
             embedded_context = (
                 f"\n{self._start_result}\n{res}\n{self._end_result}\n{focus_reminder}"
             )
-            
-            prompt += f"\n{current_chunk}\n\n"
-            print('-' * 100)
-            print(prompt)
-            time.sleep(100)
+            prompt += f"\n{current_chunk}\n{embedded_context}\n"
             print(f"\n📎 Search results added to context. Continuing reasoning...\n")
             current_chunk = ""
 
